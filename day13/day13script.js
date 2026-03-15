4d3b510da88bbd7a4c6f61bfb30e7aaf9f1501ad
Chahit Shah <shahchahit2004@gmail.com>
2026-03-15 17:48:06 +0530

    Day 22: Practiced JavaScript operators, hoisting, and conditional statements

--- day13/day13script.js
+++ day13/day13script.js
@@ -73,18 +73,129 @@
 // welcome to JS!)
 // 6. Check if msg.includes(“love”)
 
-let a = "I love Sheryians";
+// let a = "I love Sheryians";
 
-console.log(a);
+// console.log(a);
 
-console.log(a.slice(2,6));
+// console.log(a.slice(2,6));
 
-console.log(a.split(""));
+// console.log(a.split(""));
 
-console.log(a.replace("love","fuck"));
+// console.log(a.replace("love","fuck"));
 
-console.log(a.includes("love"));
+// console.log(a.includes("love"));
 
-let name = "Harsh"
+// let name = "Harsh"
 
-console.log(`heyyyyy ${name}`);
+// console.log(`heyyyyy ${name}`);
+
+
+// 1. Basic Operators (Arithmetic, Assignment, Increment, Decrement,
+// Comparison, Logical, Bitwise)
+// a. Create two numbers a = 10 and b = 3.
+// Perform and log: a + b, a - b, a * b, a / b, a % b.
+
+// let a= 10;
+// let b = 3;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// b. Write: let x = 5; x = x + 3;
+// Now rewrite the same using +=.
+// Do the same for -=, *=, /=.
+
+// let x= 5;
+// x%=3;
+// console.log(x)
+// c. let count = 5;
+// Use count++ and log value before and after.
+// Repeat for count–.
+
+// let count = 5;
+// console.log(count);
+// count++;
+// console.log(count);
+// d. Compare two values: 5 == “5” and 5 === “5”.
+// Observe difference.
+// e. Check if 10 is greater than 5, less than 20, and equal to 10.
+// f. Try logical AND and OR:
+
+// console.log(10 > 5 || 10 >20 || 10 === 10);
+// true && false
+// true || false
+// !(true)
+// g. Predict the result of:
+// (5 > 3 && 10 > 8),
+// (5 > 3 || 10 < 8)
+// h. Bitwise (light intro):
+// Evaluate 5 & 1 and 5 | 1.
+
+// console.log(5 & 1)
+// Write result and your observation (no deep explanation needed now).
+
+// 3. Conditional Operators (if, else, else-if, ternary, switch)
+// a. Take input using prompt for age.
+// If age > 18 → log “Adult”
+// Else → log “Minor”.
+
+//  let a = prompt("enter your grade ");
+
+// console.log( (a > 18)?"Adult":"Minor");
+// b. Write a program:
+// If marks >= 90 → “A grade”
+// Else if marks >= 75 → “B grade
+// // Else if marks >= 50 → “C grade”
+// // Else → “Fail”
+// if( a >=90){
+//     console.log("Grade A");
+// }
+// else if ( a >=75)
+// {
+//     console.log("B grade");
+// }
+// else if ( a>=50)
+// {
+//     console.log("C Grade");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// c. Create a variable city = “Bhopal”.
+// If city is “Bhopal” → log “MP”
+// Else if city is “Delhi” → log “Capital”
+// Else → log “Unknown City”
+
+// let city = "Banswara";
+// if( city == "Bhopal")
+// {
+//     console.log("MP");
+// }
+// else if (city == "Delhi")
+// {
+//     console.log("Capital");
+// }
+// else{
+//     console.log("Unknow city");
+// }
+// d. Use ternary operator:
+// Let score = 40.
+// If score > 35 → “Pass” else “Fail” using a ternary.
+// e. Convert this if-else into a ternary:
+// if (temperature > 30) { “Hot” } else { “Pleasant” }
+
+// Assignment - 29 Oct 2
+
+// f. Write a switch case:
+// Take day number (1 to 7).
+// Print the day name.
+// Default case: “Invalid Day”.
+// g. Using logical operators in condition:
+// If age > 18 and country == “India” → log “Eligible for Vote”
+// Else → “Not Eligible”
+
+
+
