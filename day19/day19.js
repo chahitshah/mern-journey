958e846f2c5807451565dfb8e19cd4b8f1fbba94
Chahit Shah <shahchahit2004@gmail.com>
2026-03-26 17:41:14 +0530

    Today i learned and practise Spotlight effect

---
+++ day19/day19.js
@@ -0,0 +1,8 @@
+let main = document.querySelector("main");
+
+main.addEventListener("mousemove", function(dets){
+    console.log(dets.clientX, dets.clientY);
+
+    document.body.style.setProperty("--x", dets.clientX + "px");
+    document.body.style.setProperty("--y", dets.clientY + "px");
+});
