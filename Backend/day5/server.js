b576f321636f80451941a2e05d87c6b0dd49ff3f
Chahit Shah <shahchahit2004@gmail.com>
2026-05-28 02:02:43 +0530

    feat(day-58): add status codes, json responses and setup mongodb atlas connection

---
+++ Backend/day5/server.js
@@ -0,0 +1,6 @@
+const app = require("../../Backend/day5/srs/app")
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
---
+++ Backend/day5/srs/app.js
@@ -0,0 +1,44 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes =[]
+
+app.post("/notes",(req,res)=>{
+
+    notes.push(req.body)
+
+    res.status(201).json({
+        message:"Note created successfully"
+    })
+})
+
+
+app.get("/notes",(req,res)=>{
+
+    res.status(200).json({
+        notes:notes
+    })
+})
+
+
+app.delete("/notes/:index",(req,res)=>{
+    res.status(204).json({
+        message:"Note delete Successfully"
+    })
+})
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[ req.params.index].desc=req.body.desc
+
+    res.status(200).json({
+        message:"notes updated successfully"
+    })
+})
+
+
+
+
+module.exports = app
